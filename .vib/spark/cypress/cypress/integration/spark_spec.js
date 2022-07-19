7b2fe851c6c9fefe519c675a8911e6673d897eac
michield <michield@vmware.com>
2022-07-19 15:24:05 +0200

    Change Cypress test URL

--- .vib/spark/cypress/cypress/integration/spark_spec.js
+++ .vib/spark/cypress/cypress/integration/spark_spec.js
@@ -7,6 +7,6 @@ it('checks the status', () => {
 
 it('checks if the amount of workers is correct', () => {
   const expectedWorkers = 2;
-  cy.visit('http://localhost');
+  cy.visit('/');
   cy.contains(`Alive Workers: ${expectedWorkers}`);
 });
